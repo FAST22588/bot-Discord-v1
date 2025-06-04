9a66080dbb3edecd2c7779c27fe7e22100b42af2
FAST22588 <iloveyouv100@gmail.com>
2025-06-04 19:53:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from keep_alive import server_on
 
 # ===== Configuration =====
 TOKEN = os.getenv("DISCORD_TOKEN")
-CHANNEL_ID = 1379036193525862460         # ห้องส่งคลิป
+CHANNEL_ID = 1379397123614965770         # ห้องส่งคลิป
 LOG_CHANNEL_ID = 1378977947054247957     # ห้อง log
 TARGET_CHANNEL_ID = 1379036193525862460  # ห้องที่ให้ใช้ !เมนู ได้
 COUNTDOWN_TIME = 10
